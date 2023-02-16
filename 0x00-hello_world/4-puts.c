f88c5362baec5469cebb4ee8f554e462e48c1bce
Aniede Chibundu David <ndubest56@gmail.com>
2023-02-16 13:15:27 -0800

    first c file on ALX

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * program for outpu
+ * t
+ * main: entry point that return void
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
