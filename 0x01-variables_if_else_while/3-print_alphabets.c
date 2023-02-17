62628334344936a57d6bbc65a02ede202e21d536
Aniede Chibundu David <ndubest56@gmail.com>
2023-02-17 09:15:03 -0800

    print both lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Return 0
+ */
+
+int main(void)
+{
+	int ch, CH;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (CH = 'A'; CH <= 'Z'; CH++)
+	{
+		putchar(CH);
+	}
+
+	putchar(10);
+
+	return(0);
+}
