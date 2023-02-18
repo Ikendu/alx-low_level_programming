c6d97793e8c0d4cc9c3bf57cffd0933e93873954
Aniede Chibundu David <ndubest56@gmail.com>
2023-02-17 20:56:28 -0800

    working

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,17 @@ int main(void)
 
 	for (i = 10; i <= 19; i++)
 	{
-		for(j = 10; j <= 19; j++)
+		for (j = 10; j <= 19; j++)
 		{
-			putchar((i % 10) + '0');
-			putchar((1 % 10) + '0');
-			if(i != 19 || j != 19)
+			if ((j % 10) > (i % 10))
 			{
-				putchar(',');
-				putchar(' ');
+				putchar((i % 10) + '0');
+				putchar((1 % 10) + '0');
+				if (i != 19 || j != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 
