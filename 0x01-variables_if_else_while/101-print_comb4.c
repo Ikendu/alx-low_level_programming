45aba30ead8eb29c7fd26cc6a675acca4994361b
Aniede Chibundu David <ndubest56@gmail.com>
2023-02-17 21:52:31 -0800

    combination of two digit numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,16 +14,16 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <=9; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			for (k = 0; k<=9; k++)
+			for (k = 0; k<= 9; k++)
 			{
-				if(k > j && j > i)
+				if (k > j && j > i)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-					if(i != 7 || j != 8 || k != 9)
+					if (i != 7 || j != 8 || k != 9)
 					{
 						putchar(',');
 						putchar(' ');
