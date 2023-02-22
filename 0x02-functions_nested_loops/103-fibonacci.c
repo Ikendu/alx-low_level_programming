aaba7828e2d9ff6963c387768e585ddf8ab7f562
Aniede Chibundu David <ndubest56@gmail.com>
2023-02-22 04:36:26 -0800

    correct output checking

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,18 +11,18 @@ int main(void)
 	long fb1 = 0, fb2 = 1, sum;
 	float total;
 
-	while(1)
+	while (1)
 	{
 		sum = fb1 + fb2;
 		if (sum > 4000000)
 			break;
 
 		if ((sum % 2) == 0)
-				total += sum;
+			total += sum;
 		fb1 = fb2;
 		fb2 = sum;
 	}
 	printf("%.0f\n", total);
 
 	return (0);
-}
+} 
