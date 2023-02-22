52e533906dd10bc0b665966046b43143f8727ee4
Aniede Chibundu David <ndubest56@gmail.com>
2023-02-22 04:25:47 -0800

    correct output checking

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	long i, fb1 = 0, fb2 = 1, sum;
+	long fb1 = 0, fb2 = 1, sum;
 	float total;
 
 	while(1)
@@ -17,12 +17,11 @@ int main(void)
 		if (sum > 4000000)
 			break;
 
-		if ( (sum % 2) == 0)
+		if ((sum % 2) == 0)
 				total += sum;
 		fb1 = fb2;
 		fb2 = sum;
 	}
 	printf("%.0f\n", total);
 
-	return (0);
-}
+	return (0)
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
  /**
-  * jack_bauer - Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+  * Jack_bauer - Prints every minute of the day of Jack Baue
   *
   * @i: gets the hour
   * @j: get the minutes
