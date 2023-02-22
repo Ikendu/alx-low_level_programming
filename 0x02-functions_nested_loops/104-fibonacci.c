076e286434e56b154f21c86caac9a6c0249379f6
Aniede Chibundu David <ndubest56@gmail.com>
2023-02-22 08:00:21 -0800

    fibonacci sequence number

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,46 +8,26 @@
 
 int main(void)
 {
-	int count;
-	unsigned long fb1 = 0, fb2 = 1, sum;
-	unsigned long a, a2, b, b2;
-	unsigned long x, y;
+	int count = 2;
 
-	for (count = 0; count < 92; count++)
+	float a = 1;
+	float b = a + 1;
+	float c = a + b;
+
+	printf("%.0f, ", a);
+	printf("%.0f, ", b);
+	while (count < 98)
 	{
-		sum = fb1 + fb2;
-		printf("%lu, ", sum);
-
-		fb1 = fb2;
-		fb2 = sum;
-	}	
-	
-	a = fb1 / 10000000000;
-	a2 = fb2 / 10000000000;
-	b = fb1 % 10000000000;
-	b2 = fb2 % 10000000000;
-
-	for (count = 93; count < 99; count++)
-	{
-		x = a + b;
-		y = a2 + b2;
-
-		if (a2 + b2 > 9999999999)
-		{
-			x += 1;
-			y %= 10000000000;
-		}
-		printf("%lu%lu", x, y);
-		if (count != 98)
-			printf(", ");
-
+		count++;
+		printf("%.0f", c);
 		a = b;
-		a2 = b2;
-		b = x;
-		b2 = y;
+		b = c;
+		c = a + b;
+		if (count < 98)
+		{
+			printf(", ");
+		}
 	}
-
 	printf("\n");
 	return (0);
-
 }
