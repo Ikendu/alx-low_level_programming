a757a7c3ce47fbf21abd07a4581b5865e7bb42d9
Aniede Chibundu David <ndubest56@gmail.com>
2023-02-23 09:41:18 -0800

    testing recent works

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - multply by 10
+ *
+ */
+void more_numbers(void)
+{
+	int n;
+	int x;
+
+	for (n = '0'; n <= 9; n++)
+	{
+		for (x = '0'; x <= '14'; x++)
+		{
+			_putchar('x');
+		}
+	}
+	putchar('\n');
+}
+
