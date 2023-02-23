3395ded39ce7722407549accfac34929aa95aac0
Aniede Chibundu David <ndubest56@gmail.com>
2023-02-23 08:23:45 -0800

    check digit entries

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - checkes if an entry is a digit
+ * @c: an entry to determine
+ *
+ * Return: gives 1 is digit or 0 if not
+ */
+int _isdigit(c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
