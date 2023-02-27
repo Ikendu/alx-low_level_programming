a7571727395fd66f957b822ec46b649af480d3e8
Aniede Chibundu David <ndubest56@gmail.com>
2023-02-27 11:40:16 -0800

    array format printout

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - output of integer elements
+ * @a: The array of integers
+ * @n: elements parameter to cross check
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n i++)
+	{
+		printf("%d", a[i]);
+	if (i = n - 1)
+		continue;
+
+	printf(", ");
+	}
+	printf("\n");
+}
