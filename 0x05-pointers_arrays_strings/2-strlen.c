8ad6681356ddd108b2fd56ecbcf8326cb4713930
Aniede Chibundu David <ndubest56@gmail.com>
2023-02-27 09:52:50 -0800

    returns the length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of string in vlue
+ * 
+ * @s: variable to check for length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
