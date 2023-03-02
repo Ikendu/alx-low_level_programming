243f32c509e1b1315ee199a469bcaf0c651628ee
Aniede Chibundu David <ndubest56@gmail.com>
2023-03-02 09:21:30 -0800

    redo concate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,26 @@
-#include "main.h"
+#include <main.h>
 /**
- * _strcat - concats two strings
- * @dest: destination string
- * @src: source string
+ * _strcat - concatenates two strings
+ * @dest: destination.
+ * @src: source.
  *
- * Return: character value returned
+ * Return: the pointer to dest.
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int count = 0, count2 = 0;
 
-	for (j = 0; dest[j] != '\0'; j++)
-	{}
-
-	for (i = 0; src[i] != '\0'; i++)
+	while (*(dest + count) != '\0')
 	{
-		dest[j + i] = src[i];
+		count++;
 	}
-	dest[j + i] = '\0';
-
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
