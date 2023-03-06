65c9193239b9a54807cf42f0910e7f4b393d3c5b
Aniede Chibundu David <ndubest56@gmail.com>
2023-03-06 09:27:59 -0800

    debugginh and testing

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,23 @@
 #include "main.h"
+
 /**
  * print_chessboard - board space printing
  * @a: array 
- * Return: returns board space
+ * Return: nothing
  */
+
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j = 0;
+	unsigned int i, m = 0;
 
 	for (i = 0; i < 64; i++)
 	{
 		if (i % 8 == 0 && i != 0)
 		{
-			j = i;
+			m = i;
 			_putchar('\n');
 		}
 		_putchar(a[i / 8][i - m]);
 	}
-	_putchar(a[i / 8][i - m]);
+	_putchar('\n');
 }
