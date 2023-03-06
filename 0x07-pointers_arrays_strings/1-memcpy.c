bd22e8c54b573599511950a2658898bd7c17c5df
Aniede Chibundu David <ndubest56@gmail.com>
2023-03-06 06:58:02 -0800

    pointer for making copies

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+include "main.h"
+/**
+ * _memcpy - copies items from src to dest
+ * @dest: the destination
+ * @src: the source
+ * @n: number of rounds
+ *
+ * Return: returns the dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
