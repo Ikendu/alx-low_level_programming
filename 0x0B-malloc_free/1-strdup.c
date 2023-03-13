e176259ef2c5f5d42b19ac53700218efe68a54b8
Aniede Chibundu David <ndubest56@gmail.com>
2023-03-13 07:35:58 -0700

    malloc function

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	dup = malloc(sizeof(char) * (i + 1));
-	
+
 	if (dup == NULL)
 		return (NULL);
 
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,7 +24,9 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i]; i++)
 		str[j++] = s1[i];
+
 	for (i = 0; s2[i]; i++)
 		str[j++] = s2[i];
 
 	return (str);
+}
