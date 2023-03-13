8b71441e156830928921df91c50ffcbc16da734e
Aniede Chibundu David <ndubest56@gmail.com>
2023-03-13 04:07:14 -0700

    malloc free project

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_array - creat array of charater
+ * @size: length of character
+ * @c: the character to print
+ * Return: the character of NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size i++)
+	{
+		array[i] = c;
+	}
+	return (array);
