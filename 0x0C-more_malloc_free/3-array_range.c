2a23c26241aed521ccbd0ce585efa6a1ea531771
Aniede Chibundu David <ndubest56@gmail.com>
2023-03-16 10:22:48 -0700

    rang of integer

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,33 +1,27 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * array_range - range of arrray
- * @min: minimum range
- * @max: maximum range
- * Return: pointer or null
+ * array_range - creates an array of integers.
+ * @min: minimum value.
+ * @max: maximum value.
+ *
+ * Return: pointer to the newly created array.
  */
 int *array_range(int min, int max)
 {
-	int i, m;
-	int *ptr;
+	int *ar;
+	int i;
 
 	if (min > max)
 		return (NULL);
-	m = 0;
-	for (i = min; i <= max; i++)
-		m++;
-	ptr = malloc(sizeof(int) * m);
 
-	if (ptr == NULL)
+	ar = malloc(sizeof(*ar) * ((max - min) + 1));
+
+	if (ar == NULL)
 		return (NULL);
-	i = 0;
-	while (min < max)
-	{
-		ptr[i] = min;
-		i++;
-		min++;
-	}
-	return (ptr);
+
+	for (i = 0; min <= max; i++, min++)
+		ar[i] = min;
+
+	return (ar);
 }
