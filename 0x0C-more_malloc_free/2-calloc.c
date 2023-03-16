937ed50821d4ba6d8b3ff408d1cd34c58ea7171f
Aniede Chibundu David <ndubest56@gmail.com>
2023-03-16 08:14:38 -0700

    calloc realloction22

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,22 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _calloc - allocate array to function using malloc
- * @nmemb: number of items
- * @size: size of memory
- * Return: void or the pointer
+ * _calloc - calloc function
+ * @nmemb: number of elements to allocate
+ * @size: size of bytes to allocate
+ * Return: pointer or void
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ch;
 	unsigned int i;
 
-	if(nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ch = malloc(nmemb * size);
 	if (ch == NULL)
 		return (NULL);
-
 	for (i = 0; i < nmemb * size; i++)
 		ch[i] = 0;
 
