ed28e184b01c582b2d3b009d1ee9f714837e9d2b
Aniede Chibundu David <ndubest56@gmail.com>
2023-03-31 09:24:48 +0100

    prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
+ * @h: single linked list variable.
  * Return: number of elements in the list.
  */
 
