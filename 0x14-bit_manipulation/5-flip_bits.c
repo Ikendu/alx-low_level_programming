728fd0488e0305b1012bef8a416d1fc2bc3ef65b
Aniede Chibundu <ndubest56@gmail.com>
2023-04-10 12:07:47 -0700

    number of bit to flip before access

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int xbit;
 
-	for (nbits = 0; n || m; n >>= 1, m >>= 1)
+	for (xbit = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			xbit++;
