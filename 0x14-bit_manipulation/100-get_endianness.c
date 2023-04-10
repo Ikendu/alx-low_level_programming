ff88073d43c30f823f5d0557514fcff464d6ad92
Aniede Chibundu <ndubest56@gmail.com>
2023-04-10 12:17:41 -0700

    get endianess

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - checks for endianess
+ *
+ * Return: 0 for big endian and 1 for little endian
+ */
+int get_endianness(void)
+}
+	unsigned int v;
+	char *ch;
+
+	v = 1;
+	ch = (char *) &v;
+
+	return ((int)*ch);
+}
